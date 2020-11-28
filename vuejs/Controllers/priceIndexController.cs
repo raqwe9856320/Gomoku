17c8ca8a3e2d0895462d6ec40c72031cba56f65e
瑞陽 林 <raqwe9856320@gmail.com>
2020-11-29 02:25:18 +0800

    可輸入倒數計時時間的五子棋vue.js

---
+++ vuejs/Controllers/priceIndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vuejs.Controllers
+{
+    public class priceIndexController : Controller
+    {
+        // GET: priceIndex
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
